f823cd88e5a78a6decb8bc7a4500f8e31762fcd2
abqabq <abqabq@users.noreply.github.com>
2015-08-18 18:02:44 -0400

    Update ReaderWriterLock.cpp

--- ReaderWriterLock.cpp
+++ ReaderWriterLock.cpp
@@ -27,3 +27,22 @@ void write_lock() {
 void write_unlock() {
   pthread_mutex_unlock(write_mutex);
 }
+
+
+int main(void) {
+    if (pthread_mutex_init(&count_mutex, NULL) != 0)
+    {
+        printf("\n count_mutex init failed\n");
+        return 1;
+    }
+    
+    if (pthread_mutex_init(&write_mutex, NULL) != 0)
+    {
+        printf("\n write_mutex init failed\n");
+        return 1;
+    }
+    
+    pthread_mutex_unlock(&count_mutex);
+    pthread_mutex_unlock(&write_mutex);
+}
+
